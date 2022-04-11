3cff99a0c8ee8b6397cc03b01e7a031dd06f3b75
Jon Bird <attid@Jons-Laptop>
2022-04-11 02:25:17 -0600

    starting on JS

--- assets/js/script.js
+++ assets/js/script.js
@@ -6,4 +6,33 @@ var marks = 0;
 var answer = [];
 var timer;
 
-// main read function
+// main ready function
+
+$(document).ready(function(){
+    $('#finish').hide();
+    $('#Result').hide();
+    $('highscore').hide();
+
+    buttons_manager();
+
+// Create Function
+    function buttons_manager(){
+        if(count > 0){
+            $('#prev').show;
+            if(count == 4){
+                $('#next').hide();
+                $('#finish').show();
+            }
+            else{
+                $('#next').show();
+            }
+        }
+        else {
+            $('#prev').hide();
+        }
+    }
+
+    function adding_Questions(data,i){
+        
+    }
+})
